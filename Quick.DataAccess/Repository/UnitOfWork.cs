49ec7c0feb676a8681534d5779f31fe4f48ea18a
Acer <Acer@DESKTOP-D31LNM4>
2024-05-17 00:12:43 +0300

    Add project files.

---
+++ Quick.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Quick.DataAccess.Repository;
+using Quick.DataAccess.Repository.IRepository;
+using Quick.DataAccess.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
+        
+        
+        public UnitOfWork(ApplicationDbContext db) 
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
+           
+           
+        }
+
+        
+
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
